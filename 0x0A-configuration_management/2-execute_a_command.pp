17b3d72e6d718ef62767784fd72838068e230dfb
damadet <adedokundamilare41@gmail.com>
2022-10-14 13:14:08 -0700

    kill me now

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,6 @@
 #command to kill a process named kill me now
-exec { 'pkill'
-  command => 'pkill -f killmenow',
-  path => '/usr/bin/:/usr/local/bin/:/bin/',
+exec {
+  'pkill -f killmenow':
+ # command => 'pkill -f killmenow',
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
