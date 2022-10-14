6542b6c15a65e769788b6b07e865caf272a641bd
damadet <adedokundamilare41@gmail.com>
2022-10-14 12:20:30 -0700

    made the module to create a file puppet-lint compliant

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 #create a file in /tmp.
 file { 'school':
-	content => 'I love Puppet',
-	owner => 'www-data',
-	group => 'www-data',
-	mode => '0744'
-	path => '/tmp/school',
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744'
+  path    => '/tmp/school',
 }
