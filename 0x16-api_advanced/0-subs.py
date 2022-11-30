b4b612ebacf8f839540de3170bb4432d7b8ccb50
damadet <adedokundamilare41@gmail.com>
2022-11-30 01:32:02 +0100

    api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python3
-'''queries and returns corresponding result from the Reddit API'''
+"""
+module - 0-subs.py
+contains a function with prototype
+def number_of_subscribers(subreddit)
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    '''
-    Description:
-    - queries and returns the number of subscibers
-      (not active users, total subscribers) for a given subreddit.
-    - returns 0 if an invalid subreddit is given
-    '''
-    endpoint = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'My User Agent 1.0'}
-    res = requests.get(endpoint, headers=headers)
-
-    if res.status_code != 200:
+    """
+    returns the number ofsubscribers
+    (not active users, total subscribers) for a given subreddit.
+    """
+    if not subreddit:
         return 0
-    return res.json()['data']['subscribers']
+    if type(subreddit) is not str:
+        return 0
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'user-agent': 'nehe-browser/1.1.1'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+    data = response.json().get('data', {})
+    subscribers = data.get('subscribers', 0)
+    return subscribers
